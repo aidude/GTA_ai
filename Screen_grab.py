4477a77baffea75669221ab923c04cde4f81ab2d
champ <amritansh2007@gmail.com>
2017-09-18 22:56:31 +0530

    Added Keyboard inputs

--- Screen_grab.py
+++ Screen_grab.py
@@ -9,10 +9,10 @@ def screen_record():
     while(True):
         # 800x600 windowed mode for GTA 5, at the top left position of your main screen.
         # 40 px accounts for title bar. 
-        printscreen =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
-        print('Frames {} per second'.format(1/(time.time()-last_time)))
+        screen_capture =  np.array(ImageGrab.grab(bbox=(0,40,800,640)))
+        print('{} Frames per second'.format(1/(time.time()-last_time)))
         last_time = time.time()
-        cv2.imshow('window',cv2.cvtColor(printscreen, cv2.COLOR_BGR2RGB))
+        cv2.imshow('window',cv2.cvtColor(screen_capture, cv2.COLOR_BGR2RGB))
         if cv2.waitKey(25) & 0xFF == ord('q'):
             cv2.destroyAllWindows()
             break
