8f7820867822bdad6121b1638cfc4505e6877098
champ <amritansh2007@gmail.com>
2017-09-29 23:07:59 +0530

    Movement presets for vehicles

---
+++ neural_net/moves.py
@@ -0,0 +1,76 @@
+# Locomotion moves for vehicles.
+@ author = amritansh
+
+
+
+def straight():
+    PressKey(W)
+    ReleaseKey(A)
+    ReleaseKey(D)
+    ReleaseKey(S)
+
+
+def left():
+    if random.randrange(0, 3) == 1:
+        PressKey(W)
+    else:
+        ReleaseKey(W)
+    PressKey(A)
+    ReleaseKey(S)
+    ReleaseKey(D)
+##    ReleaseKey(S)
+
+
+def right():
+    if random.randrange(0, 3) == 1:
+        PressKey(W)
+    else:
+        ReleaseKey(W)
+    PressKey(D)
+    ReleaseKey(A)
+    ReleaseKey(S)
+
+
+def reverse():
+    PressKey(S)
+    ReleaseKey(A)
+    ReleaseKey(W)
+    ReleaseKey(D)
+
+
+def forward_left():
+    PressKey(W)
+    PressKey(A)
+    ReleaseKey(D)
+    ReleaseKey(S)
+
+
+def forward_right():
+    PressKey(W)
+    PressKey(D)
+    ReleaseKey(A)
+    ReleaseKey(S)
+
+
+def reverse_left():
+    PressKey(S)
+    PressKey(A)
+    ReleaseKey(W)
+    ReleaseKey(D)
+
+
+def reverse_right():
+    PressKey(S)
+    PressKey(D)
+    ReleaseKey(W)
+    ReleaseKey(A)
+
+
+def no_keys():
+    if random.randrange(0, 3) == 1:
+        PressKey(W)
+    else:
+        ReleaseKey(W)
+    ReleaseKey(A)
+    ReleaseKey(S)
+    ReleaseKey(D)
