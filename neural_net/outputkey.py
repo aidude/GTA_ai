410700ed6204d2d8558c596c68afd184c8d02a17
champ <amritansh2007@gmail.com>
2017-10-01 22:17:21 +0530

    Change the resize value

--- neural_net/outputkey.py
+++ neural_net/outputkey.py
@@ -4,18 +4,7 @@ from directkeys import W, A, D, directkeys
 
 
 
-key_map = {
-    'W': [1, 0, 0, 0, 0, 0, 0, 0, 0],
-    'S': [0, 1, 0, 0, 0, 0, 0, 0, 0],
-    'A': [0, 0, 1, 0, 0, 0, 0, 0, 0],
-    'D': [0, 0, 0, 1, 0, 0, 0, 0, 0],
-    'WS': [0, 0, 0, 0, 1, 0, 0, 0, 0],
-    'WD': [0, 0, 0, 0, 0, 1, 0, 0, 0],
-    'SA': [0, 0, 0, 0, 0, 0, 1, 0, 0],
-    'SD': [0, 0, 0, 0, 0, 0, 0, 1, 0],
-    'NK': [0, 0, 0, 0, 0, 0, 0, 0, 1],
-    'default': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-}
+
 def output_keys(keys):
 	key_map = [0,0,0]
 
@@ -28,8 +17,21 @@ def output_keys(keys):
 	return key_map
 
 def output_keys_1(keys):
-	""" Generate one hot encoding and tack them together
+	""" Create one hot encoding and tack them together
 	"""
+
+	key_map = {
+    'W': [1, 0, 0, 0, 0, 0, 0, 0, 0],
+    'S': [0, 1, 0, 0, 0, 0, 0, 0, 0],
+    'A': [0, 0, 1, 0, 0, 0, 0, 0, 0],
+    'D': [0, 0, 0, 1, 0, 0, 0, 0, 0],
+    'WS': [0, 0, 0, 0, 1, 0, 0, 0, 0],
+    'WD': [0, 0, 0, 0, 0, 1, 0, 0, 0],
+    'SA': [0, 0, 0, 0, 0, 0, 1, 0, 0],
+    'SD': [0, 0, 0, 0, 0, 0, 0, 1, 0],
+    'NK': [0, 0, 0, 0, 0, 0, 0, 0, 1],
+    'default': [0, 0, 0, 0, 0, 0, 0, 0, 0],
+	}
 	if ''.join(keys) in key_map:
         return key_map[''.join(keys)]
 	return key_map['default']
