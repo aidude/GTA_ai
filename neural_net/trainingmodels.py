26805ce4aab4ee3d02f38307a35e604109561993
champ <amritansh2007@gmail.com>
2017-10-07 22:04:49 +0530

    Added loop to run through epochs

--- neural_net/trainingmodels.py
+++ neural_net/trainingmodels.py
@@ -28,3 +28,18 @@ PREV_MODEL = ''
 MODEL_NAME = ''
 
 model = googlenet(WIDTH, HEIGHT, 3, LR, output=9, model_name = MODEL_NAME)
+
+if LOAD_MODEL:
+    model.load(PREV_MODEL)
+print('We have loaded a previous running model !!')
+
+
+for epoch in range(EPOCHS):
+	data_order = [i for i in range(1, FILE_I_END + 1)]
+	shuffle(data_order)
+	for count,i in enumerate(data_order):
+		try:
+			file_name = 'D:/phase10-random-padded/training_data-{0}.npy'.format(i)
+            # full file info
+            train_data = np.load(file_name)
+		print('training_data-{0}.npy'.format(i), len(train_data))
