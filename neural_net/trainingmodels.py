3f435f00352ccd916c7d78234021769c15220dac
champ <amritansh2007@gmail.com>
2017-10-07 22:01:21 +0530

    Declaring Parameters and their values

--- neural_net/trainingmodels.py
+++ neural_net/trainingmodels.py
@@ -14,3 +14,17 @@ from models import inception_v3 as googlenet
 from random import shuffle
 
 
+FILE_END = 1080
+WIDTH = 80
+HEIGHT = 60
+
+EPOCHS = 25
+
+LR = 1e-2
+
+LOAD_MODEL = True
+
+PREV_MODEL = ''
+MODEL_NAME = ''
+
+model = googlenet(WIDTH, HEIGHT, 3, LR, output=9, model_name = MODEL_NAME)
